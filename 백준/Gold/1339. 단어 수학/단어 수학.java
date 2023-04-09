5a5a9e180e5ea4661d7705d92ffad6a0acf1c912
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2023-04-09 21:03:01 +0900

    [Gold IV] Title: 단어 수학, Time: 136 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+public class Main {
+
+
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		String[] word = new String[N];
+		int[] alphabet = new int[26];//A~Z
+		
+		
+		for(int i=0; i<N; i++) {
+			String line = br.readLine();
+			int digit = (int) Math.pow(10, line.length()-1); //자리수
+			for(int j=0; j<line.length(); j++) {
+				int idx = line.charAt(j) - 'A';
+				alphabet[idx] += digit;
+				digit/=10;
+			}
+		}
+		
+		//정렬
+		Arrays.sort(alphabet);
+		/*for(int i=0; i<alphabet.length; i++) {
+			System.out.println(alphabet[i]);
+		}*/
+
+		int result = 0;
+		for(int i=25; i>=17; i--) {
+			result += alphabet[i]*(i-16);
+		}
+		System.out.println(result);
+	}
+
+}
