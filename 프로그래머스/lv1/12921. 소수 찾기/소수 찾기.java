584c6ded604682f0fee7150fbb6e9eebd01ba90d
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2023-04-17 20:09:53 +0900

    [level 1] Title: 소수 찾기, Time: 179.17 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0; 
+        for(int i=2; i<=n; i++){
+            if(isPrime(i)) answer++;
+        }
+        return answer;
+    }
+    public boolean isPrime(int num){
+        if(num<2) return false;
+        for(int i=2; i*i<=num; i++){
+            if(num%i==0) return false;
+        }
+        return true;
+    }
+}
