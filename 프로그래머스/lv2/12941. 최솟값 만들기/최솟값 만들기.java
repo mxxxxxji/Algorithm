2ebb909b16cc90c431edc0c3a593b2919e5eba04
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2023-04-27 20:26:29 +0900

    [level 2] Title: 최솟값 만들기, Time: 3.28 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        Integer[] b = new Integer[B.length];
+        for(int i=0; i<b.length; i++){
+            b[i] = B[i];
+        }
+        Arrays.sort(A);
+        Arrays.sort(b, Collections.reverseOrder());
+        for(int i=0; i<A.length; i++){
+            answer+=A[i]*b[i];
+        }
+        return answer;
+    }
+}
