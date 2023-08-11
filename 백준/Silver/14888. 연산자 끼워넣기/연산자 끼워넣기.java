d365471fd4299fa7f44d3b529096e378e8ead34b
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2023-08-11 16:29:54 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 84 ms, Memory: 13632 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,68 +1,69 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
 import java.util.StringTokenizer;
- 
+
 public class Main {
- 
-	public static int MAX = Integer.MIN_VALUE;	// 최댓값 
-	public static int MIN = Integer.MAX_VALUE;	// 최솟값 
-	public static int[] operator = new int[4];	// 연산자 개수 
-	public static int[] number;					// 숫자 
-	public static int N;						// 숫자 개수 
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		N = Integer.parseInt(br.readLine());
-		number = new int[N];
- 
-		// 숫자 입력 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
-			number[i] = Integer.parseInt(st.nextToken());
-		}
- 
-		// 연산자 입력 
-		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < 4; i++) {
-			operator[i] = Integer.parseInt(st.nextToken());
-		}
- 
-		dfs(number[0], 1);
- 
-		System.out.println(MAX);
-		System.out.println(MIN);
- 
-	}
- 
-	public static void dfs(int num, int idx) {
-		if (idx == N) {
-			MAX = Math.max(MAX, num);
-			MIN = Math.min(MIN, num);
-			return;
-		}
- 
-		for (int i = 0; i < 4; i++) {
-			// 연산자 개수가 1개 이상인 경우
-			if (operator[i] > 0) {
- 
-				// 해당 연산자를 1 감소시킨다.
-				operator[i]--;
- 
-				switch (i) {
- 
-				case 0:	dfs(num + number[idx], idx + 1);	break;
-				case 1:	dfs(num - number[idx], idx + 1);	break;
-				case 2:	dfs(num * number[idx], idx + 1);	break;
-				case 3:	dfs(num / number[idx], idx + 1);	break;
- 
-				}
-				// 재귀호출이 종료되면 다시 해당 연산자 개수를 복구한다.
-				operator[i]++;
-			}
-		}
-	}
- 
+    static int N;
+    static int[] arr;
+    static int[] operator = new int[4];
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+ //       operator = new int[4];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        // +, -, *, /
+        for(int i=0; i<4; i++){
+            operator[i] = Integer.parseInt(st.nextToken());
+        }
+        dfs(arr[0], 1);
+        System.out.println(max);
+        System.out.println(min);
+    }
+    public static void dfs(int num, int idx){
+        if(idx == N){
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+            return;
+        }
+        for(int i=0; i<4; i++){
+            if(operator[i]>0){
+                operator[i]--;
+//                switch (i) {
+//
+//                    case 0:
+//                        dfs(num+arr[idx], idx+1); break;
+//                    case 1:
+//                        dfs(num-arr[idx], idx+1); break;
+//                    case 2:
+//                        dfs(num*arr[idx], idx+1); break;
+//                    case 3:
+//                        dfs(num/arr[idx], idx+1); break;
+//                }
+//                operator[i]++;
+                if(i==0) {
+                    dfs(num+arr[idx], idx+1);
+                }
+                else if(i==1){
+                    dfs(num-arr[idx], idx+1);
+                }
+                else if(i==2){
+                    dfs(num*arr[idx], idx+1);
+                }
+                else{
+                    dfs(num/arr[idx], idx+1);
+                }
+                operator[i]++;
+
+            }
+        }
+    }
 }
