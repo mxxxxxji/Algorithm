249efe0e0cdd095ff5650684a4e749974a6332cc
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2023-08-24 14:37:30 +0900

    [Silver I] Title: NM과 K (1), Time: 1308 ms, Memory: 13252 KB -BaekjoonHub

--- 백준/Silver/18290. NM과 K （1）/NM과 K （1）.java
+++ 백준/Silver/18290. NM과 K （1）/NM과 K （1）.java
@@ -1,35 +1,52 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, K, answer;
-    static int[][] arr;
-    static boolean[][] visited;
+    static int N, M, K;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, -1, 0, 1};
+    static int[][] map;
+    static boolean[][] visited;
+    static int max = Integer.MIN_VALUE;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visited = new boolean[N + 1][M + 1];
+        max = Integer.MIN_VALUE;
 
-    public static void solve(int x, int y, int idx, int sum) {
-        if (idx == K) {
-            answer = Math.max(answer, sum);
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dfs(0, 0, 0, 0);
+        System.out.println(max);
+    }
+    public static void dfs(int r, int c, int depth, int value){
+
+        if(depth==K){
+            max = Math.max(max, value);
             return;
-        } else {
-            for (int i = x; i < N; i++) {
-                for (int j = y; j < M; j++) {
-                    if (!visited[i][j]) {
-                        if (check(i, j)) {
-                            visited[i][j] = true;
-                            solve(x, y, idx + 1, sum + arr[i][j]);
-                            visited[i][j] = false;
-                        }
+        }
+        for(int i=r; i<N; i++){
+            for(int j=c; j<M; j++){
+                if (!visited[i][j]) {
+                    if (check(i, j)) {
+                        visited[i][j] = true;
+                        dfs(r, c, depth+1, value+map[i][j]);
+                        visited[i][j] = false;
                     }
                 }
             }
         }
-
     }
-
     public static boolean check(int x, int y) {
         boolean flag = true;
         for (int i = 0; i < 4; i++) {
@@ -43,26 +60,5 @@ public class Main {
             }
         }
         return flag;
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        arr = new int[N][M];
-        visited = new boolean[N + 1][M + 1];
-        answer = Integer.MIN_VALUE;
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < M; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        solve(0, 0, 0, 0);
-        System.out.println(answer);
     }
 }
