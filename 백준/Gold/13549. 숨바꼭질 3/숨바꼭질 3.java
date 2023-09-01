382279b5396ad9294151b150df669e786faf6a60
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2023-09-01 15:37:55 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 104 ms, Memory: 16456 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,44 +1,65 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-	static int[] dx = {-1, 1};
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		
-        bfs(n, k);
-	}
-	
-	static void bfs(int start, int destination) {
-		Queue<int[]> q = new LinkedList<>();
-		boolean[] check = new boolean[100_001];
-		q.add(new int[]{start,0});
-		check[start] = true;
-		
-		while(!q.isEmpty()) {
-			int[] p = q.poll();
-			int pos = p[0];
-			int move = p[1];
-			if(pos == destination) {
-				System.out.println(move);
-				return;
-			}
-			int jump = pos*2;
-			if(jump < 100001 && !check[jump]) {
-				check[jump] = true;
-				q.add(new int[]{jump,move});
-			}
+    static int N, K;
+    static int[] dx = {-1, 1};
 
-			for(int i=0; i<2; i++) {
-				int next = pos + dx[i];
-				if (next>=0 && next <100001 && !check[next]) {
-					check[next] = true;
-					q.add(new int[]{next,move+1});
-				}
-			}
-		}
-	}
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+
+        find(N, K);
+    }
+    static void find(int start, int end){
+        Queue<Node> pq = new LinkedList<>();
+        boolean[] visited = new boolean[100001];
+        pq.add(new Node(start, 0));
+        visited[start] = true;
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
+            if(cur.x == end){
+                System.out.println(cur.time);
+                return;
+            }
+            int jump = cur.x*2;
+            if(jump< 100001 && !visited[jump]) {
+                visited[jump]= true;
+                pq.offer(new Node(jump, cur.time));
+            }
+                        int back=cur.x-1;
+            if(back >= 0 && back < 100001&& !visited[back]){
+                visited[back]= true;
+                pq.add(new Node(back, cur.time+1));
+            }
+            int go = cur.x+1;
+            if(go >= 0 &&go < 100001 && !visited[go]){
+                visited[go]= true;
+                pq.add(new Node(go, cur.time+1));
+
+            }
+//            for(int i=0; i<2; i++) {
+//                int next = cur.x + dx[i];
+//                if (next>=0 && next <100001 && !visited[next]) {
+//                    visited[next] = true;
+//                    pq.add(new Node(next,cur.time+1));
+//                }
+//            }
+
+        }
+    }
+    static class Node{
+        int x;
+        int time;
+        public Node(int x, int time){
+            this.x = x;
+            this.time = time;
+        }
+    }
 }
