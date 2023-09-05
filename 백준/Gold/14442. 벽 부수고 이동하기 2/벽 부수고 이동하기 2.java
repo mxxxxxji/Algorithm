9d8ea862a83ddac6a539acdcba91825aacdac691
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2023-09-05 15:15:37 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1048 ms, Memory: 320088 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -1,17 +1,17 @@
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, K;
+    static int N,M,K;
     static int[][] map;
-    static int[][] visited;
+    static int[][] dist;
     static int[][] dir = {{-1,0}, {1,0}, {0,-1}, {0,1}};
-    static int res;
-    public static void main(String[] args) throws Exception {
+    static int result=-1;
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -19,49 +19,46 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        map = new int[N+1][M+1];
-        visited = new int[N+1][M+1];
-        for(int i=0; i<N+1; i++){
-            Arrays.fill(visited[i], Integer.MAX_VALUE);
-        }
-        for(int i=1; i<N+1; i++){
-            String[] s = br.readLine().split("");
-            for(int j=1; j<M+1; j++){
-                map[i][j] = Integer.parseInt(s[j-1]);
+        map = new int[N][M];
+        dist = new int[N][M];
+
+        for(int i=0; i<N; i++){
+            String s = br.readLine();
+            for(int j=0; j<M; j++){
+                map[i][j] = s.charAt(j)-'0';
+                dist[i][j] = Integer.MAX_VALUE;
             }
         }
-        res = -1;
         bfs();
-
-        System.out.println(res);
-
+        System.out.println(result);
     }
     public static void bfs(){
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{1, 1, 0, 1});
-        visited[1][1] = 0;
+        q.add(new int[]{0,0,0,1});
+        dist[0][0] = 0; // 부순 벽의 개수
         while(!q.isEmpty()){
             int[] p = q.poll();
-            int r = p[0];
-            int c = p[1];
-            int b = p[2];
-            int dist = p[3];
-            if(r == N && c == M) {
-                res=dist; break;
+            int r = p[0]; int c = p[1];
+            int block = p[2]; int count = p[3];
+            if(r==N-1&& c==M-1){
+                result = count;
+                break;
             }
             for(int i=0; i<4; i++){
                 int nr = r + dir[i][0];
                 int nc = c + dir[i][1];
-                if(nr<1 || nc<1 || nr>N || nc>M) continue;
-                if(visited[nr][nc]<=b) continue;
-                if(map[nr][nc]==1) { // 벽일때
-                    if(b+1<visited[nr][nc] && b<K) {
-                        visited[nr][nc] = b+1;
-                        q.add(new int[]{nr, nc, b+1, dist+1});
+
+                if(nr<0 || nc<0 || nr>N-1|| nc>M-1) continue;
+//                if(dist[nr][nc]>K) continue;
+                if(dist[nr][nc]<=block) continue;
+                if(map[nr][nc] == 1){
+                    if(dist[nr][nc]>block+1 && block<K){
+                        dist[nr][nc] = block+1;
+                        q.add(new int[]{nr, nc, block+1, count+1});
                     }
                 }else{
-                    visited[nr][nc] = b;
-                    q.add(new int[]{nr, nc, b, dist+1});
+                    dist[nr][nc] = block;
+                    q.add(new int[]{nr, nc, block, count+1});
                 }
             }
         }
