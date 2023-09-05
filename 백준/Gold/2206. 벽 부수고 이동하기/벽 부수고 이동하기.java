c7d0dcb1f8fb996844ac1dc1319400bd7897d13e
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2023-09-05 14:39:22 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1148 ms, Memory: 212076 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -7,18 +7,18 @@ import java.util.StringTokenizer;
 public class Main {
     static int N, M;
     static int[][] map;
-    static int[][] visited;
-    static int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
-    static int answer;
+    static int[][] dir = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+    static int[][] visited; // 공사횟수
+    static int result=-1;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+
         map = new int[N][M];
         visited = new int[N][M];
-
         for(int i=0; i<N; i++){
             String[] s = br.readLine().split("");
             for(int j=0; j<M; j++){
@@ -26,42 +26,37 @@ public class Main {
                 visited[i][j] = Integer.MAX_VALUE;
             }
         }
-        answer = Integer.MAX_VALUE;
-        bfs(0, 0);
-        if(answer==Integer.MAX_VALUE) System.out.println(-1);
-        else System.out.print(answer);
-
+        bfs();
+        System.out.println(result);
     }
-    private static void bfs(int a, int b){
+    static void bfs(){
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{a, b, 1, 0});
-        visited[a][b] = 0; // 공사횟수
+        q.add(new int[]{0, 0, 0, 1});
+        visited[0][0]=0;
         while(!q.isEmpty()){
             int[] p = q.poll();
-            int r = p[0];
-            int c = p[1];
-            int dist = p[2];
-            int block = p[3];
-            if(p[0]==N-1 && p[1]==M-1){
-                answer =  dist; break;
+            int r = p[0]; int c = p[1]; int block = p[2]; int cnt = p[3];
+            if(r==N-1&& c==M-1) {
+                result = cnt;
+                break;
             }
             for(int i=0; i<4; i++){
                 int nr = r + dir[i][0];
                 int nc = c + dir[i][1];
-                //System.out.println(flag);
+
                 if(nr<0 || nc<0 || nr>N-1 || nc>M-1) continue;
-
+                // 벽을 부수고 지나갔을 경우 visit=1, 벽을 부수지 않고 오늘 녀석 block=0
                 if(visited[nr][nc]<=block) continue;
-
-                // 벽이 아닐때
-                if(map[nr][nc] == 0){
-                    visited[nr][nc] = block;
-                    q.add(new int[]{nr, nc, dist+1, block});
-                }else{ // 벽 일때
-                    if(block == 0){
-                        visited[nr][nc] = block + 1;
-                        q.add(new int[]{nr, nc, dist+1, block+1});
+             //   System.out.println(r+", "+c+", "+block+", "+cnt);
+             //   System.out.println(nr+", "+nc);
+                if(map[nr][nc]==1){
+                    if(block==0){
+                        visited[nr][nc] = block+1;
+                        q.add(new int[]{nr, nc, block+1, cnt+1});
                     }
+                }else {
+                    visited[nr][nc] = block;
+                    q.add(new int[]{nr, nc, block, cnt+1});
                 }
             }
         }
