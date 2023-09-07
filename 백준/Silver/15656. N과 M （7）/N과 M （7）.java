e2c66cd4a1e0d8a38f6d13ddef89611ac38f9f1f
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2023-09-07 18:39:06 +0900

    [Silver III] Title: N과 M (7), Time: 572 ms, Memory: 219984 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.java
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -4,44 +4,38 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int N;
-	static int M;
-	static int[] out;
-	static int[] arr;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		out = new int[M];
-		arr = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(arr);
-		
-		rec(0);
-		System.out.print(sb);
-	}
-	public static void rec(int depth) {
-		if(depth==M) {
-			for(int i=0; i<M; i++) {
-				sb.append(out[i]).append(' ');
-			}
-			sb.append('\n');
-			return;
-		}
-		for(int i=0; i<N; i++) {
-			out[depth] = arr[i];
-			rec(depth+1);
-		}
-	}
-
+    static int N, M;
+    static int[] arr;
+    static int[] number;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        number = new int[N];
+        visited = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            number[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(number);
+        dfs(0);
+        System.out.print(sb);
+    }
+    static void dfs(int depth){
+        if(depth==M){
+            for(int num : arr){
+                sb.append(num).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for(int i=0; i<N; i++){
+            arr[depth] = number[i];
+            dfs(depth+1);
+        }
+    }
 }
