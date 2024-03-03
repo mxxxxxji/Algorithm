fcc03b94c2c73bda4e1c2ac069ac4258e2e18a43
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2024-03-03 15:04:14 +0900

    [Silver II] Title: 초콜릿 식사, Time: 76 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Silver/2885. 초콜릿 식사/초콜릿 식사.java
+++ 백준/Silver/2885. 초콜릿 식사/초콜릿 식사.java
@@ -1,38 +1,31 @@
-import java.io.*;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner(System.in);
-         	int k = scan.nextInt();	
-		int size = 1;
-		int count = 0;
-		int size2 = 0;	
-		
-		while(size < k) {
-			size = size * 2;
-			size2 = size;
-		}
-		// K크기의 초콜릿을 얻기 위해서는 그와 같거나 큰 초콜릿이 필요하다.
-        // 2제곱근으로 증가한다고 하니 2씩 곱해주어 가장작은
-        // 초콜릿 크기를 구한다.
-        
-        
-		while(k > 0) {
-			if(k>=size) {
-				k-=size;
-			}else {
-				size /= 2;
-				count++;
-			}
-		}
-		//최소 초콜릿의 크기가 K보다 클경우 반으로 쪼개고
-        //count를 하나씩 더해준다.
-        //K 가 0보다 작아지면 해당 반복문을 중단한다.
-        
-        
-		System.out.println(size2+" " + count);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = 1;
+        int cnt = 0;
+        while(true){
+            if(n>=K) break;
+            n*=2;
+        }
+        // 최소 초콜릿의 크기
+        sb.append(n).append(" ");
 
+        while(K>0){
+            // 최소 초콜릿이 더 크면 쪼개기
+            if(K<n){
+                n/=2;
+                cnt++;
+            }else{ // 더 작으면 안쪼개도 되니까 필요한 양(K)에서 뻬기
+                K-= n;
+            }
+        }
+        sb.append(cnt).append('\n');
+        System.out.println(sb);
     }
 
 }
