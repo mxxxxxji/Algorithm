f820bcdb78374ecaf5ffc084c6cb33df33583e2b
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2024-05-02 15:07:47 +0900

    [Gold IV] Title: 연구소 2, Time: 240 ms, Memory: 41876 KB -BaekjoonHub

--- 백준/Gold/17141. 연구소 2/연구소 2.java
+++ 백준/Gold/17141. 연구소 2/연구소 2.java
@@ -1,116 +1,125 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static int N, M;
     static int[][] map;
-    static int[][] dir = {{-1,0}, {1,0}, {0,-1}, {0,1}};
-    static ArrayList<Node> list; // 가능한 위치를 넣음
+    static List<Node> list; // 바이러스를 놓을 수 있는 칸(2)
     static boolean[] visited;
     static Node[] virusPos;
+    static int[][] copyMap;
     static int minTime = Integer.MAX_VALUE;
-    public static void main(String[] args) throws Exception{
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+    static int[][] dir = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         map = new int[N][N];
-
-        virusPos = new Node[M];
-
-
         list = new ArrayList<>();
-        int cnt = 0;
+        int emptySpace = 0;
+        int countVirus = 0; // 바이러스를 놓을 수 있는 칸
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<N; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j]==2) {
+                if(map[i][j]==2){
                     list.add(new Node(i, j));
-                    cnt++;
+                    countVirus++;
+                    map[i][j]=0;
+                }
+                if(map[i][j]==0){
+                    emptySpace++;
                 }
             }
         }
-        visited = new boolean[cnt];
-        putVirus(0, 0);
-        if(minTime==Integer.MAX_VALUE) System.out.println(-1);
-        else System.out.println(minTime-1);
-    }
+        //System.out.println(emptySpace+", " +countVirus);
+        visited = new boolean[countVirus];
+        virusPos = new Node[M];
+        copyMap = new int[N][N];
 
-    // 모든 연구소 칸이 감염되었는가
-    private static boolean check(int[][] temp, boolean[][] visited){
+        // 퍼질 공간이 없다면
+        if(emptySpace==0||emptySpace==M) System.out.println(0);
+        else{
+            putVirus(0, 0);
+            if(minTime == Integer.MAX_VALUE) System.out.println(-1);
+            else System.out.println(minTime);
+        }
+
+    }
+    static int[][] copyMap(){
+        int[][] copy = new int[N][N];
         for(int i=0; i<N; i++){
             for(int j=0; j<N; j++){
-                if(temp[i][j]!=1 &&!visited[i][j]) return false;
+                copy[i][j] = map[i][j];
+            }
+        }
+        return copy;
+    }
+    //모든 연구소가 감염되었는가
+    static boolean checkMap(int[][]curMap){
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                if(curMap[i][j]==0) return false;
             }
         }
         return true;
     }
-    // 배열 복사
-    private static int[][] copyMap(){
-        int[][] copy = new int[N][N];
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                copy[i][j] = (map[i][j]==2 ? 0 : map[i][j]);
-            }
-        }
-        for(int i=0; i<M; i++){
-            copy[virusPos[i].r][virusPos[i].c] = 2;
-        }
-        return copy;
-    }
-    private static void spreadVirus(int[][] temp){
-
+    static void spreadVirus(int[][] curMap){
         Queue<Node> q = new LinkedList<>();
-        boolean[][] visited = new boolean[N][N];
+        boolean[][] check = new boolean[N][N];
+
         // 큐에 바이러스 넣기
         for(int i=0; i<M; i++){
-            q.add(new Node(virusPos[i].r, virusPos[i].c));
-            visited[virusPos[i].r][virusPos[i].c] = true;
+            q.add(virusPos[i]);
+            curMap[virusPos[i].r][virusPos[i].c] = 2;
+            check[virusPos[i].r][virusPos[i].c] = true;
         }
+
+//        for(int i=0; i<N; i++){
+//            for(int j=0; j<N; j++){
+//                System.out.print(curMap[i][j]);
+//            }
+//            System.out.println();
+//        }
+//        System.out.println();
         int time = 0;
         while(!q.isEmpty()){
-            if(minTime<=time) break;
-            int size = q.size();
-            for(int i=0; i<size; i++){ // 시작 위치가 여러개
+            if(minTime<=time) break; // 이미 최소 시간보다 커짐
+            int len = q.size();
+            for(int i=0; i<len; i++){
                 Node node = q.poll();
-
                 for(int j=0; j<4; j++){
                     int nr = node.r + dir[j][0];
                     int nc = node.c + dir[j][1];
 
                     if(nr<0 || nc<0 || nr>N-1 || nc>N-1) continue;
-                    if(visited[nr][nc]) continue;
-                    if(temp[nr][nc]!=1) {
-                        temp[nr][nc] = 2;
-                        visited[nr][nc] =true;
-                        q.add(new Node(nr, nc));
-                    }
+                    if(curMap[nr][nc]==1) continue; // 벽이 아니거나 이미 바이러스 있는 경우
+                    if(curMap[nr][nc]==2) continue;
+                    curMap[nr][nc] = 2;
+                    q.add(new Node(nr, nc));
+
                 }
             }
             time++;
+            if(checkMap(curMap)) break;
         }
-        if(check(temp, visited)) {
-            minTime = Math.min(time, minTime);
-        }
+        // 퍼질 수 있는 곳은 다 퍼졌지만 빈 공간이 남았을 수도 있기 때문에 검사
+        if(checkMap(curMap))minTime = Math.min(minTime,time);
+
     }
-    // 가능한 바이러스 위치 조합
-    private static void putVirus(int depth, int start){
+    static void putVirus(int depth, int start){
         if(depth == M){
-            int[][] copy = copyMap();
-            spreadVirus(copy);
+            int[][] temp = copyMap();
+            spreadVirus(temp);
             return;
         }
         for(int i=start; i<list.size(); i++){
             if(visited[i]) continue;
-            virusPos[depth] = list.get(i);
             visited[i] = true;
+            virusPos[depth] = list.get(i);
             putVirus(depth+1, i+1);
             visited[i] = false;
         }
@@ -122,5 +131,6 @@ public class Main {
             this.r = r;
             this.c = c;
         }
+
     }
 }
