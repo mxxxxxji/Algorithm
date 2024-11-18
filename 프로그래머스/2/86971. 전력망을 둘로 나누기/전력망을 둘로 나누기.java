80449d44d78881dab817ef221d3b9d640fe6781f
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2024-11-18 11:08:40 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 13.89 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class Solution {
+    static int map[][];
+
+    public int solution(int n, int[][] wires) {
+        int answer = n;
+        map = new int[n+1][n+1];
+        
+        for(int i=0; i<wires.length; i++){
+            int a = wires[i][0];
+            int b = wires[i][1];
+            map[a][b] = 1;
+            map[b][a] = 1;
+        }
+        for(int i=0; i<wires.length; i++){
+            int a = wires[i][0];
+            int b = wires[i][1];
+            map[a][b] = 0;
+            map[b][a] = 0;
+           
+            //System.out.println()
+            answer = Math.min(answer, bfs(i+1, n));
+            
+            map[a][b] = 1;
+            map[b][a] = 1;
+        }
+        return answer;
+    }
+    public static int bfs(int x, int n){
+        int count = 1;
+        Queue<Integer> q = new LinkedList<>();
+        int[] visited = new int[n+1];
+        q.add(x);
+        visited[x] = 1;
+        while(!q.isEmpty()){
+            int p = q.poll();
+            for(int i=1; i<=n; i++){
+                if(visited[i]==0 &&map[p][i]==1){
+                    q.add(i);
+                    count++;
+                    visited[i]=1;
+                    
+                    
+                }
+            }
+        }
+        return (int)Math.abs(n-count*2);
+    }
+}
