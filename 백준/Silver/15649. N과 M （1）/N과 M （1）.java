3cd10c1ace6c3ff2f71f80b07821d789e73cd5ff
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2025-03-11 15:23:29 +0900

    [Silver III] Title: N과 M (1), Time: 148 ms, Memory: 20408 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,34 +1,44 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static int[] arr;
+    static int n, m;
+    static StringBuilder sb;
     static boolean[] visited;
-    public static void main(String[] args)throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new int[M];
-        visited = new boolean[N+1];
-        dfs(0);
+        sb = new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+
+        visited = new boolean[n+1];
+
+        combination(new int[m], 0);
+        System.out.println(sb);
     }
-    static void dfs(int depth){
-        if(depth==M){
-            for(int i=0; i<arr.length; i++){
-                System.out.print(arr[i]+" ");
+    static void combination(int[]arr, int depth){
+        if(depth == m){
+            for(int num : arr){
+                sb.append(num);
+                sb.append(' ');
             }
-            System.out.println();
+            sb.append("\n");
             return;
         }
-        for(int i=1; i<N+1; i++){
-            if(visited[i]) continue;
-            visited[i] = true;
-            arr[depth] = i;
-            dfs(depth+1);
-            visited[i] = false;
+
+        for(int i=1; i<=n; i++){
+            if(!visited[i]){
+                arr[depth] = i;
+                visited[i] = true;
+                combination(arr, depth+1);
+                visited[i] = false;
+
+            }
         }
     }
 }
