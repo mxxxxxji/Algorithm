813b8d95e9f3406559b2233e2e36386e583add8d
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2025-03-11 15:38:54 +0900

    [Silver III] Title: N과 M (2), Time: 64 ms, Memory: 11564 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,38 +1,35 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
     static StringBuilder sb = new StringBuilder();
-    static int[] arr;
-    static boolean[] visited;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new int[M];
-        visited = new boolean[N+1];
-        dfs(1,0);
-        System.out.print(sb);
+        permutation(1, n, m, new int[m], new boolean[n+1], 0);
+        System.out.println(sb);
     }
-    static void dfs(int start, int depth){
-        if(depth==M){
-            for(int i=0; i<arr.length; i++){
-                sb.append(arr[i]).append(' ');
+    static void permutation(int start,  int n, int m, int[] arr, boolean[] visited, int depth){
+        if(depth == m){
+            for(int num : arr){
+                sb.append(num).append(' ');
             }
-            sb.append('\n');
+            sb.append("\n");
             return;
         }
-        for(int i=start; i<N+1; i++){
+        for(int i = start; i<=n; i++){
             if(visited[i]) continue;
             visited[i] = true;
             arr[depth] = i;
-            dfs(i+1,depth+1);
+            permutation(i+1, n, m, arr, visited, depth+1);
             visited[i] = false;
 
         }
+
     }
 }
