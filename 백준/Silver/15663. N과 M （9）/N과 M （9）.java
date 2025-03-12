fbb198f1ea684a3d600ca82dc4c50f18b1ffc158
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2025-03-12 16:59:37 +0900

    [Silver II] Title: N과 M (9), Time: 104 ms, Memory: 17768 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,56 +1,47 @@
+
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    // 9분
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	static int N;
-	static int M;
-	static int[] out;
-	static int[] arr;
-	static boolean[] visited;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		out = new int[M];
-		arr = new int[N];
-		visited= new boolean[N];
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(arr);
-		
-		rec(0);
-		System.out.print(sb);
-	}
-	public static void rec(int depth) {
-		if(depth==M) {
-			for(int i=0; i<out.length; i++) {
-				sb.append(out[i]).append(' ');
-			}
-			sb.append('\n');
-			return;
-		}
-		int before = 0;
-		for(int i=0; i<N; i++) {
-			if(before != arr[i]) {
-				if(!visited[i]) {
-					visited[i] = true;
-					out[depth] = arr[i];
-					before = arr[i];
-					rec(depth+1);
-					visited[i] = false;
-				}
-			}
-		}
-	}
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
+        st = new StringTokenizer(br.readLine());
+        int[] temp = new int[n];
+        for(int i =0; i<n; i++){
+            temp[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(temp);
+        permutation(0, temp,  n, m, new int[m], new boolean[n],  0);
+        System.out.println(sb);
+
+    }
+    static void permutation(int start, int[] temp, int n, int m, int[] arr, boolean[] visited, int depth) {
+        if (depth == m) {
+            for (int num : arr) {
+                sb.append(num).append(' ');
+            }
+            sb.append("\n");
+            return;
+        }
+        int before = 0;
+        for (int i = 0; i < n; i++) {
+            if(before == temp[i]) continue;
+            if (visited[i]) continue;
+            arr[depth] = temp[i];
+            visited[i] = true;
+            before =  temp[i];
+            permutation(i+1, temp, n, m, arr, visited, depth + 1);
+             visited[i] = false;
+        }
+    }
 }
