2c71081bafae1829a97da670308c0499b8dcaa4d
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2025-03-12 16:01:29 +0900

    [Silver III] Title: N과 M (1), Time: 144 ms, Memory: 20352 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -4,41 +4,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static StringBuilder sb;
-    static boolean[] visited;
+    // 53
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        sb = new StringBuilder();
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
 
 
-        visited = new boolean[n+1];
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        combination(new int[m], 0);
+        combination(n, m, new boolean[n+1], new int[m], 0);
         System.out.println(sb);
+
     }
-    static void combination(int[]arr, int depth){
+    static void combination(int n, int m, boolean[] visited, int[] arr, int depth){
         if(depth == m){
             for(int num : arr){
-                sb.append(num);
-                sb.append(' ');
+                sb.append(num).append(' ');
             }
             sb.append("\n");
             return;
         }
-
         for(int i=1; i<=n; i++){
-            if(!visited[i]){
-                arr[depth] = i;
-                visited[i] = true;
-                combination(arr, depth+1);
-                visited[i] = false;
+            if(visited[i]) continue;
 
-            }
+            arr[depth] = i;
+            visited[i] = true;
+            combination(n, m, visited, arr, depth+1);
+            visited[i] = false;
         }
     }
+
 }
