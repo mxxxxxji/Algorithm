25d9d023e0e258f0fbe28358a012823c9be601fa
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2025-03-14 18:09:30 +0900

    [Silver III] Title: N과 M (4), Time: 80 ms, Memory: 13272 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -3,8 +3,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
-    // 14
+public class Main {
+    // 9
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,11 +14,11 @@ public class Main{
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        permutation( 1, n, m, new int[m], 0);
+        permutation(n, m, new int[m], 0, 1 );
         System.out.println(sb);
 
     }
-    static void permutation(int start, int n, int m, int[] arr,  int depth) {
+    static void permutation(int n, int m, int[] arr, int depth, int start) {
         if (depth == m) {
             for (int num : arr) {
                 sb.append(num).append(' ');
@@ -26,12 +26,9 @@ public class Main{
             sb.append("\n");
             return;
         }
-        for(int i=start; i<=n; i++){
-
-            arr[depth] = i;
-            permutation(i, n, m, arr,depth+1);
-
-
-        }
+       for(int i = start; i<=n; i++){
+           arr[depth] = i;
+           permutation(n, m, arr, depth+1, i);
+       }
     }
 }
