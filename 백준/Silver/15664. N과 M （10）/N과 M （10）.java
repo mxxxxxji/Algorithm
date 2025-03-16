9a0f5a018b37594f7cc7754f6a789ae1fc930a55
mxxxxxji <52437364+mxxxxxji@users.noreply.github.com>
2025-03-16 19:02:19 +0900

    [Silver II] Title: N과 M (10), Time: 64 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.java
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -1,46 +1,44 @@
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
-    // 5분 35-40
+public class Main{
+    //50-1
     static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception{
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
         int[] temp = new int[n];
-        for(int i =0; i<n; i++){
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
             temp[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(temp);
-        permutation(temp,  n, m, new int[m], new boolean[n],  0, 0);
+        func(n, m, temp, 0, new int[m], new boolean[n], 0);
         System.out.println(sb);
-
     }
-    static void permutation(int[] temp, int n, int m, int[] arr, boolean[] visited, int depth, int start) {
-        if (depth == m) {
-            for (int num : arr) {
+    static void func(int n, int m, int[] temp, int depth, int[] arr, boolean[] visited, int start){
+        if(depth == m){
+            for(int num: arr){
                 sb.append(num).append(' ');
             }
             sb.append("\n");
             return;
         }
         int before = 0;
-        for (int i = start; i < n; i++) {
-            if (before == temp[i]) continue;
-            if (visited[i]) continue;
+        for(int i=start; i<n; i++){
+            if(before==temp[i]) continue;
+            if(visited[i]) continue;
             visited[i] = true;
             arr[depth] = temp[i];
             before = temp[i];
-            permutation(temp, n, m, arr, visited, depth + 1, i + 1);
+            func(n, m, temp, depth+1, arr, visited, i+1);
             visited[i] = false;
         }
     }
